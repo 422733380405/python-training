12da9fc80a8e59d2903af2fc4946aa472af909cd
422733380405 <nandinikamini10@gmail.com>
2025-12-26 14:31:34 +0530

    Add files via upload

---
+++ number pattern.py
@@ -0,0 +1,17 @@
+
+x=int(input())
+for i in range(x):
+    for j in range(i+1):
+        print(j,end=" ")
+    print()
+
+x=int(input())
+num=65
+for i in range(x):
+    for j in range(i+1):
+        print(j+1,end=" ")
+    print()
+
+
+
+
